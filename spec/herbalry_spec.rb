91f8705c4f9534a98cb2b2f4710eb72fb049882d
Frances <mynameislucah@yahoo.com>
2018-01-16 06:30:51 -0800

    starting models specs

--- spec/herbalry_spec.rb
+++ spec/herbalry_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../app/models/herbalry'
 
 RSpec.describe Herbalry do
-
+  describe "attributes" do
+    it "respondes to name" do
+      herbalry = Herbalry.new
+      expect(herbalry).to respond_to(:name)
 end
