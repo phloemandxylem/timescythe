14d54a37651a71b2adaf258a1cb9b80ec3507587
Frances <mynameislucah@yahoo.com>
2018-01-25 07:17:15 -0800

    added user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   before_save{self.email = email.downcase if email.present}
 
+  attr_accessor :name,:email,:password
   validates :name, length:{minimum: 1, maximum:100}, presence: true
   validates :password, presence: true, length: {minimum 6}, if: "password_digest.nil?"
   validates :password, length:{minimum: 6}, allow_blank: true
+
+  def initialize (name, email, password,)
+    @name = name
+    @description = email
+    @product = password
+  end
 end
